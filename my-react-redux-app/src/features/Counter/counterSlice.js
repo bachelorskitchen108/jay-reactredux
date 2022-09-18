10a381d3cd31114baeb0caf967c845205f139b08
bachelorskitchen108 <77671705+bachelorskitchen108@users.noreply.github.com>
2022-09-18 19:24:53 +0530

    adding counter part application (#3)

---
+++ my-react-redux-app/src/features/Counter/counterSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    count: 0
+}
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment : (state) => {
+            state.count += 1;
+        },
+        decrement: (state) => {
+            state.count -= 1;
+        },
+        reset: (state) => {
+            state.count = initialState.count;
+        },
+        incByAmount: (state,action) => {
+            state.count += action.payload;
+        }
+    }
+});
+
+export const { increment, decrement, reset, incByAmount } = counterSlice.actions;
+
+export default counterSlice.reducer;
